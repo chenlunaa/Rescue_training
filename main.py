66673e591d14ee348c45598fdb38f31021bd9300
chenlunaa <2146529322@qq.com>
2025-11-25 16:39:55 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ green = [87, 116, 17]   #BGR format of green color
 brown = [70, 75, 82] #BGR format of brown color
 
 color = [
-    "yellow",
-    "pink",
     "black",
+    "pink",
     "blue",
+    "brown",
     "green",
-    "brown"
+    "yellow"
 ]
 
 lower_limit_ylw, upper_limit_ylw =bgr_to_hsv(yellow)
@@ -26,14 +26,15 @@ lower_limit_grn, upper_limit_grn =bgr_to_hsv(green)
 lower_limit_brn, upper_limit_brn =bgr_to_hsv(brown)
 
 ranges = [
-    (lower_limit_ylw, upper_limit_ylw),
-    (lower_limit_pnk, upper_limit_pnk),
     (lower_limit_blk, upper_limit_blk),
+    (lower_limit_pnk, upper_limit_pnk),
     (lower_limit_blu, upper_limit_blu),
+    (lower_limit_brn, upper_limit_brn),
     (lower_limit_grn, upper_limit_grn),
-    (lower_limit_brn, upper_limit_brn)
+    (lower_limit_ylw, upper_limit_ylw)
 ]
-img = cv2.imread('1_2.png')
+
+img = cv2.imread('1_1.png')
 hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 img_gray = cv2.GaussianBlur(img_gray, (5, 5), 0)
@@ -46,6 +47,7 @@ kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9,9))
 canny_img = cv2.Canny(img_gray, low, high)
 canny_img = cv2.morphologyEx(canny_img, cv2.MORPH_CLOSE, kernel, 4)
 contours_canny = cv2.findContours(canny_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+results = np.empty((0, 3))
 
 for cnt in contours_canny:
     j = 0
@@ -65,10 +67,13 @@ for cnt in contours_canny:
         areas = [np.count_nonzero(m) for m in mask]
         max_index = np.argmax(areas)
         cv2.putText(img, color[max_index], (w_center - 20, h_center - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
-        print(f"Color {color[max_index]}: Center=({w_center}, {h_center}), Width={w}, Height={h}")
+        # print(f"Color {color[max_index]}: Center=({w_center}, {h_center}), Width={w}, Height={h}")
+        new_row = np.array([[w_center, h_center, max_index]])
+        results = np.vstack([results, new_row])
+        cv2.circle(img, (w_center, h_center), 3, (255, 0, 0), 2, cv2.LINE_AA)
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 0), 2)
 
+print(results.astype(int))
 cv2.imshow('Image', img) 
-cv2.imwrite('./test/output_2.png', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
